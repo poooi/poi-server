8addd1847736a233a7ff8c033a15df99e02946ba
Dazzy Ding <h@dazzyd.org>
2017-09-20 11:49:51 +0800

    Add standalone hooks server

---
+++ hooks.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const ChildProcess = require('child_process')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/api/github-master-hook', async (ctx, next) => {
+  const cp = ChildProcess.spawn('./github-master-hook', {stdio: 'inherit'})
+  cp.on('close', (code) => console.log('* exit code:' + code))
+  ctx.status = 200
+  await next()
+})
+
+// Start server
+const Port = 11280
+app.use(router.routes())
+app.listen(Port, '127.0.0.1', () => {
+  console.log(`Server is listening at port ${Port}`)
+})
