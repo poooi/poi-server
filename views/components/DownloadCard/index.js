1eb6ae66dea4f9418b1e164876ed2df599a450b8
KochiyaOcean <root@kochiyaocean.org>
2018-10-18 16:42:39 +0900

    chore: update url path

--- views/components/DownloadCard/index.js
+++ views/components/DownloadCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import classNames from 'classnames'
-import semver from 'semver'
 
 import version from '../../meta/version'
 
@@ -11,48 +10,24 @@ const BASE_URI = 'https://npm.taobao.org/mirrors/poi'
 function getDownloadLink(version, target) {
   const pure = version.substring(1)
   switch (target) {
-    case 'linux-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-linux-x64.7z`
-    case 'linux-deb-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-linux-x64.deb`
-    case 'linux-rpm-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-linux-x64.rpm`
-    case 'macos-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-macos-x64.dmg`
-    case 'win-ia32':
-      return `${BASE_URI}/${version}/poi-${pure}-win-ia32.7z`
-    case 'win-ia32-setup':
-      return `${BASE_URI}/${version}/poi-${pure}-win-ia32-setup.exe`
-    case 'win-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-win-x64.7z`
-    case 'win-x64-setup':
-      return `${BASE_URI}/${version}/poi-${pure}-win-x64-setup.exe`
-    default:
-      return 'https://github.com/poooi/poi/releases'
-  }
-}
-
-function getNewVersionDownloadLink(version, target) {
-  const pure = version.substring(1)
-  switch (target) {
-    case 'linux-x64':
-      return `${BASE_URI}/${version}/poi-${pure}.7z`
-    case 'linux-deb-x64':
-      return `${BASE_URI}/${version}/poi_${pure}_amd64.deb`
-    case 'linux-rpm-x64':
-      return `${BASE_URI}/${version}/poi-${pure}.rpm`
-    case 'macos-x64':
-      return `${BASE_URI}/${version}/poi-${pure}.dmg`
-    case 'win-ia32':
-      return `${BASE_URI}/${version}/poi-${pure}-ia32-win.7z`
-    case 'win-ia32-setup':
-      return `${BASE_URI}/${version}/poi-setup-${pure}.exe`
-    case 'win-x64':
-      return `${BASE_URI}/${version}/poi-${pure}-win.7z`
-    case 'win-x64-setup':
-      return `${BASE_URI}/${version}//poi-setup-${pure}.exe`
-    default:
-      return 'https://github.com/poooi/poi/releases'
+  case 'linux-x64':
+    return `${BASE_URI}/${version}/poi-${pure}.7z`
+  case 'linux-deb-x64':
+    return `${BASE_URI}/${version}/poi_${pure}_amd64.deb`
+  case 'linux-rpm-x64':
+    return `${BASE_URI}/${version}/poi-${pure}.x86_64.rpm`
+  case 'macos-x64':
+    return `${BASE_URI}/${version}/poi-${pure}.dmg`
+  case 'win-ia32':
+    return `${BASE_URI}/${version}/poi-${pure}-ia32-win.7z`
+  case 'win-ia32-setup':
+    return `${BASE_URI}/${version}/poi-setup-${pure}.exe`
+  case 'win-x64':
+    return `${BASE_URI}/${version}/poi-${pure}-win.7z`
+  case 'win-x64-setup':
+    return `${BASE_URI}/${version}//poi-setup-${pure}.exe`
+  default:
+    return 'https://github.com/poooi/poi/releases'
   }
 }
 
@@ -64,14 +39,14 @@ class DownloadCard extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.header}>{ __('download-for') } { __(target) }</div>
-        <a href={(semver.gte(version.stable, 'v7.8.0-beta.2') ? getNewVersionDownloadLink : getDownloadLink)(version.stable, target)}>
+        <a href={getDownloadLink(version.stable, target)}>
           <button className={classNames(styles.button, styles.stable)}>
             <div>{ version.stable }</div>
             <div className={styles.description}>{ __('stable-hint') }</div>
           </button>
         </a>
         { version.betaAvailable &&
-          <a href={(semver.gte(version.beta, 'v7.8.0-beta.2') ? getNewVersionDownloadLink : getDownloadLink)(version.beta, target)}>
+          <a href={getDownloadLink(version.beta, target)}>
             <button className={classNames(styles.button, styles.beta)}>
               <div>{ version.beta }</div>
               <div className={styles.description}>{ __('beta-hint') }</div>
@@ -84,7 +59,7 @@ class DownloadCard extends React.Component {
 }
 
 DownloadCard.contextTypes = {
-  __: React.PropTypes.func
+  __: React.PropTypes.func,
 }
 
 export default DownloadCard
