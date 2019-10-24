c946a6572b3863d4b322561c6d1afbe9e36e924d
Kagami <moe.pascal@gmail.com>
2019-10-24 23:07:18 +0800

    chore: clean unused files

--- views/intl/metadata.js
+++
@@ -1,6 +0,0 @@
-export default {
-  'en-US': 'English',
-  'zh-CN': '简体中文',
-  'zh-TW': '正體中文',
-  'ja-JP': '日本語',
-}
--- views/meta/version.js
+++
@@ -1,5 +0,0 @@
-export default {
-  stable: 'v10.3.0',
-  beta: 'v10.4.0-beta.0',
-  betaAvailable: true,
-}
